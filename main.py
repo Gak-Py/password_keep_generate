82ef9d3e511244ada4595dd412136ae45966d977
jipdesign-jp <jipdesigninfo@gmail.com>
2022-05-06 10:59:56 +0900

    update  added search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from tkinter import *
 from tkinter import messagebox
 import random
@@ -26,24 +27,59 @@ def pass_add():
     website = website_input.get()
     user = user_input.get()
     password = pass_input.get()
+    new_data = {
+        website : {
+            "user" : user,
+            "password" : password,
+        }
+    }
     if website == "" or user == "" or password =="":
         messagebox.showinfo("Alert", "Fill All Input!")
 
     else:
-        with open("pass_list.csv", mode="a") as data:
-            writer = csv.writer(data)
-            writer.writerow([website, user, password])
-        messagebox.showinfo("Alert", "Password Added!")
-        website_input.delete(0, END)
-        pass_input.delete(0, END)
-        user_input.delete(0, END)
-        website_input.focus()
+        try:
+            with open("pass_list.json", "r") as json_data:
+                data = json.load(json_data)
+        except FileNotFoundError:
+            with open("pass_list.json", "w") as json_data:
+                json.dump(new_data, json_data, indent=4)
+
+        else:
+            data.update(new_data)
+            with open("pass_list.json", "w") as json_data:
+                json.dump(data, json_data, indent=4)
+        # with open("pass_list.csv", mode="a") as data:
+        #     writer = csv.writer(data)
+        #     writer.writerow([website, user, password])
+        finally:
+            messagebox.showinfo("Alert", "Password Added!")
+            website_input.delete(0, END)
+            pass_input.delete(0, END)
+            user_input.delete(0, END)
+            website_input.focus()
+
+def pass_search():
+    website = website_input.get()
+    try:
+        with open("pass_list.json") as data:
+            df = json.load(data)
+    except FileNotFoundError:
+        messagebox.showinfo("Alert", "Create first password.")
+    else:
+        if website in df:
+            messagebox.showinfo("Alert", f'user:\n{df[website]["user"]}\npassword:\n{df[website]["password"]}')
+        else:
+            messagebox.showinfo("Alert", "No User/Password exists.")
+
 
 website_area = Label(text="website", font=("Futura", 15, "italic"),anchor="w")
 website_area.grid(row=1, column=0)
 
-website_input = Entry(textvariable="website url", width=40)
-website_input.grid(row=1, column=1, columnspan=2, sticky="w")
+website_input = Entry(textvariable="website url", width=26)
+website_input.grid(row=1, column=1, sticky="w")
+
+search_btn = Button(text="search", command=pass_search, width=10)
+search_btn.grid(row=1, column=2, sticky="w")
 
 user_area = Label(text="user/email", font=("Futura", 15, "italic"),anchor="w")
 user_area.grid(row=2, column=0)
@@ -53,12 +89,12 @@ user_input.grid(row=2, column=1, columnspan=2, sticky="w")
 
 
 pass_area = Label(text="password", font=("Futura", 15, "italic"),anchor="w")
-pass_area.grid(row=3, column=0)
+pass_area.grid(row=3, column=0, )
 
-pass_gen_btn = Button(text="generate", command=gen_pass)
+pass_gen_btn = Button(text="generate", command=gen_pass, width=10)
 pass_gen_btn.grid(row=3, column=2, sticky="w")
 
-pass_input = Entry(width=25)
+pass_input = Entry(width=26)
 pass_input.grid(row=3, column=1, sticky="w")
 
 add_btn = Button(text="add", command=pass_add)
